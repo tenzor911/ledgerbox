37561bcb5bf4aa537e7cb0fcb8c461a47d7b069e
tenzor <tenzor@localhost>
2013-03-20 12:43:06 +0400

    making authorization window

--- src/mainpak/AuthorizationWindowView.java
+++ src/mainpak/AuthorizationWindowView.java
@@ -5,6 +5,10 @@ import javax.swing.JFrame;
 public class AuthorizationWindowView extends JFrame {
     
     AuthorizationWindowView() {
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        
         
         
         
