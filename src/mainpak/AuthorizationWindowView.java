24642bf5c1c2904858afb086dfcccd5206aca259
tenzor <tenzor@localhost>
2013-03-20 19:06:23 +0400

    making GUI auth

--- src/mainpak/AuthorizationWindowView.java
+++ src/mainpak/AuthorizationWindowView.java
@@ -1,16 +1,53 @@
 package mainpak;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
-public class AuthorizationWindowView extends JFrame {
+public class AuthorizationWindowView extends JFrame { 
     
     AuthorizationWindowView() {
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(300, 200);
+        setVisible(true);
+        setResizable(false);
+        setLayout(null);
+        add(FieldForLogin);
+        add(FieldForPassw);
+        add(LogInButton);
+        add(ExitAuthFormButton);
+        setLocationRelativeTo(null); 
+        settingAuthorizationForm();
+        
+    } 
+    
+    JTextField FieldForLogin = new JTextField();
+    JTextField FieldForPassw = new JTextField();
+    JButton LogInButton = new JButton("login");
+    JButton ExitAuthFormButton = new JButton("exit");
+   
+    SpringLayout layout = new SpringLayout();
+    
+    void settingAuthorizationForm() {
+        
+        FieldForLogin.setBounds(50, 40, 200, 20);   
+        FieldForLogin.setVisible(true);
+        
+        FieldForPassw.setBounds(50, 65, 200, 20);   
+        FieldForPassw.setVisible(true);
+        
+        LogInButton.setBounds(50, 90, 97, 20);
+        LogInButton.setVisible(true);
+        
+        ExitAuthFormButton.setBounds(153, 90, 97, 20);
+        ExitAuthFormButton.setVisible(true);
+        
         
         
         
         
     }
+    
+  
 }
--- src/mainpak/MainClass.java
+++ src/mainpak/MainClass.java
@@ -1,8 +1,10 @@
 package mainpak;
 
 public class MainClass {
+    
     public static void main(String args[]) {
         DataBaseConnection DataBaseMainConnection = new DataBaseConnection();
+        AuthorizationWindowView AuthorizationWindow = new AuthorizationWindowView();
         DataBaseMainConnection.DataBaseConnectionInitiating();
     }
     
